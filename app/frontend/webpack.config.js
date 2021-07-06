4714b89264d9e8112ad9b4b11ca2ac7fc5aead65
Ryota <onumaryotajh@gmail.com>
2021-07-06 23:59:58 +0900

    tsの環境構築完了

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -1,38 +1,34 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, "src/index.jsx"),
-    output: {
-       // 出力先のディレクトリ
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, './dist')
-    },
-    devServer: {
-        compress: true,
-        contentBase: path.join(__dirname, "dist"),
-        watchContentBase: true,
-        hot: true,
-        port: 3035,
-    },
-    module: {
-      rules: [{
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    // 出力先のディレクトリ
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
+    hot: true,
+    port: 3035,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts | tsx) ?$/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/react"
-            ]
-          }
-        }
-      }]
-    },
-    target: ["web", "es5"],
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
+          loader: "awesome-typescript-loader",
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: ["web", "es5"],
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
 };
--- app/frontend/webpack.dev.config.js
+++ app/frontend/webpack.dev.config.js
@@ -1,40 +1,35 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, "src/index.jsx"),
-    output: {
-       // 出力先のディレクトリ
-      path: path.resolve(__dirname, './dist'),
-      filename: 'bundle.js',
-      publicPath: 'http://localhost:3500/',
-      // path: path.resolve(__dirname, '../static/')
-    },
-    devServer: {
-        compress: true,
-        contentBase: path.join(__dirname, "dist"),
-        watchContentBase: true,
-        hot: true,
-        port: 3035,
-    },
-    module: {
-      rules: [{
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/react"
-            ]
-          }
-        }
-      }]
-    },
-    target: ["web", "es5"],
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    // 出力先のディレクトリ
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "http://localhost:3500/",
+    // path: path.resolve(__dirname, '../static/')
+  },
+  devServer: {
+    compress: true,
+    contentBase: path.join(__dirname, "dist"),
+    watchContentBase: true,
+    hot: true,
+    port: 3035,
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts もしくは .tsx の場合
+        test: /\.tsx?$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader",
+      },
+    ],
+  },
+  target: ["web", "es5"],
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+  },
 };
